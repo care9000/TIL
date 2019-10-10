47080e31e0dd005afd5556ec46e1e7c4ce502cb6
park <care9000@naver.com>
2019-10-10 17:57:11 +0900

    2019-10-10|목요일

--- 03_Django/04_django_crud_review/articles/urls.py
+++ 03_Django/04_django_crud_review/articles/urls.py
@@ -3,12 +3,16 @@ from . import views
 
 app_name = 'articles'
 
+
 urlpatterns = [
     path('', views.index,name='index'), #GET  요청
     # path('new/',views.new,name='new'),
     path('create/', views.create,name='create'), #GET(new),POST(create)
-    path('<int:pk>/', views.detail,name='detail'),#GET
-    path('<int:pk>/delete/',views.delete,name='delete'),#POST
+    path('<int:article_pk>/', views.detail,name='detail'),#GET
+    path('<int:article_pk>/delete/',views.delete,name='delete'),#POST
     # path('<int:pk>/edit/',views.edit,name='edit'),
-    path('<int:pk>/update/', views.update,name='update'),# GET(edit),POST(update)
+    path('<int:article_pk>/update/', views.update,name='update'),# GET(edit),POST(update)
+    path('<int:article_pk>/comments/', views.comment_create, name='comments_create'),
+    path('<int:article_pk>/comments/<int:comment_pk>/delete/', views.comments_delete, name='comments_delete'),
+    path('<int:article_pk>/comments/<int:comment_pk>/update/', views.comments_update, name='comments_update')
 ]
--- 03_Django/04_django_crud_review/articles/views.py
+++ 03_Django/04_django_crud_review/articles/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.core.exceptions import ValidationError
-from .models import Article
+from .models import Article, Comment
 from IPython import embed
 
 def index(request):
@@ -47,27 +47,28 @@ def create(request):
     # Article.objects.create(title=title, content=content)
     return redirect(f'/articles/{article.pk}')
     '''
-def detail(request, pk):
-    article = Article.objects.get(pk=pk)
-    context = {'article': article}
+def detail(request, article_pk):
+    article = Article.objects.get(pk=article_pk)
+    comments = article.comments.all()
+    context = {'article': article,'comments': comments } #'comments':article.comments.all()}
     return render(request,'articles/detail.html', context )
 
-def delete(request, pk):
+def delete(request, article_pk):
     if request.method =='POST':
-        article =Article.objects.get(pk=pk)
+        article =Article.objects.get(pk=article_pk)
         article.delete()
         return redirect('articles:index')
     else:
-        return redirect('articles:index')
+        return redirect('articles:index',article.pk)
 
 def edit(request, pk):
     article = Article.objects.get(pk=pk)
     context = {'article':article }
     return render(request,'articles/edit.html', context)
 
-def update(request, pk):
+def update(request, article_pk):
     # embed()
-    article = Article.objects.get(pk=pk)
+    article = Article.objects.get(pk=article_pk)
     if request.method == 'POST':
         article.title = request.POST.get('title')
         article.content = request.POST.get('content')
@@ -76,3 +77,32 @@ def update(request, pk):
         return redirect('articles:detail', article.pk)
     context = {'article':article }
     return render(request, 'articles/update.html',context)
+
+
+def comment_create(request, article_pk):
+    article = Article.objects.get(pk=article_pk)
+    if request.method == 'POST':
+        content = request.POST.get('content')
+        comment = Comment(article=article, content=content)
+        comment.save()
+        return redirect('articles:detail', article.pk)
+        
+    else:
+        return redirect('articles:detail', article.pk)
+
+def comments_delete(request, article_pk, comment_pk):
+    if request.method == 'POST':
+        comment = Comment.objects.get(pk=comment_pk)
+        comment.delete()
+        return redirect('articles:detail', article_pk)
+    return redirect('articles:detail', article_pk)
+
+def comments_update(request,article_pk, comment_pk):
+    comment = Comment.objects.get(pk=comment_pk)
+    if request.method == 'POST':
+        comment.content = request.POST.get('content')
+        comment.save()
+        return redirect('article:detail', article_pk)
+    else:
+        context = {'comment': comment}
+        return render(request,'articles/comment_update.html',context)
