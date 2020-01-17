f64ffbac88a135d8e33ce8df0b16b60747993858
박교열 <care9000@naver.com>
2020-01-17 11:51:15 +0900

    2020-01-17|오전

--- winter_1/0104_AL/2206_벽_부수고_이동하기.py
+++ winter_1/0104_AL/2206_벽_부수고_이동하기.py
@@ -3,60 +3,60 @@ sys.stdin = open('2206_벽_부수고_이동하기.txt')
 
 import collections
 def bfs(i, j):
-    global result
     q = collections.deque([])
     q.append([i, j, 1, 1])
-    visited[i][j] = 1
+    visited_cnt_1[i][j] = 1
+    visited_cnt_0[i][j] = 1
     while len(q):
-        location = q.popleft()
-        x = location[0]
-        y = location[1]
-        distance = location[2]
-        chance = location[3]
-        if distance > result:
-            return
+        i, j, cnt, reach = q.popleft()
         for k in range(4):
-            x_tem = x + dx[k]
-            y_tem = y + dy[k]
-            if ispass(x_tem, y_tem):
-                if my_map[x_tem][y_tem] == 0:
-                    visited[x_tem][y_tem] = distance + 1
-                    if x_tem == N - 1 and y_tem == M - 1:
-                        result = distance + 1
-                        return
-                    q.append([x_tem, y_tem, distance + 1, chance])
+            i_tem = i + dx[k]
+            j_tem = j + dy[k]
+            if -1 < i_tem < N and -1 < j_tem < M:
+                #cnt 가 1 일때 와 0 일때 구분
+                if cnt:
+                    # 벽이 아닐 경우
+                    if mini_map[i_tem][j_tem] == 0:
+                        if visited_cnt_1[i_tem][j_tem] > reach + 1:
+                            visited_cnt_1[i_tem][j_tem] = reach + 1
+                            q.append([i_tem, j_tem, 1, reach + 1])
+                        if visited_cnt_0[i_tem][j_tem] > reach + 1:
+                            visited_cnt_0[i_tem][j_tem] = reach + 1
+
+                    # 벽 일 경우
+                    else:
+                        if visited_cnt_0[i_tem][j_tem] > reach + 1:
+                            visited_cnt_0[i_tem][j_tem] = reach + 1
+                            q.append([i_tem, j_tem, 0, reach + 1])
+
+                # cnt 가 0 일 경우
                 else:
-                    if chance:
-                        if visited[x_tem][y_tem] == 0:
-                            visited[x_tem][y_tem] = distance + 1
-                            if x_tem == N - 1 and y_tem == M - 1:
-                                result = distance + 1
-                                return
-                            q.append([x_tem, y_tem, distance + 1, chance - 1])
-
-
-def ispass(i, j):
-    if -1 < i < N and -1 < j < M:
-        return True
-    return False
+                    if mini_map[i_tem][j_tem] == 0:
+                        if visited_cnt_0[i_tem][j_tem] > reach + 1:
+                            visited_cnt_0[i_tem][j_tem] = reach + 1
+                            q.append([i_tem, j_tem, 0, reach + 1])
 
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-
 N, M = map(int, input().split())
-dummy = [input() for _ in range(N)]
 
-my_map = [[0 for _ in range(M)] for _ in range(N)]
-wall_location_list = []
+dummy = list(input() for _ in range(N))
+mini_map = [[0 for _ in range(M)] for _ in range(N)]
 for i in range(N):
     for j in range(M):
-        my_map[i][j] = int(dummy[i][j])
+        mini_map[i][j] = int(dummy[i][j])
 
-result = 987654321
-visited = [[0 for _ in range(M)] for _ in range(N)]
+
+
+visited_cnt_1 = [[987654321 for _ in range(M)] for _ in range(N)]
+
+visited_cnt_0 = [[987654321 for _ in range(M)] for _ in range(N)]
+
+
 bfs(0, 0)
+result = min(visited_cnt_0[-1][-1], visited_cnt_1[-1][-1])
 if result != 987654321:
     print(result)
 else:
